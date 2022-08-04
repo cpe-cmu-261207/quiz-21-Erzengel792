65e397dfbe0969b6379fad919ab48b25895a2419
Erzengel792 <tanadol_boat@hotmail.com>
2022-08-04 14:10:53 +0700

    Sent

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
 }
+
+const btnRandom = document.querySelector("#btn-random");
+const imgProfile = document.querySelector("#img-profile");
+
+btnRandom.onclick = async () => {
+  const resp2 = await axios.get(
+    "https://randomuser.me/api/portraits/women/11.jpg"
+  );
+  imgProfile.src = resp2.data.result.picture.large;
+};
